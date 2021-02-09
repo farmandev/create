b864d6529e39a16720cf099803b20f384fde64ff
farmandev <mdali.lioncoders@gmail.com>
2021-02-09 11:56:31 +0600

    new

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 (function($) {
     "use strict";
 
-    //===== Prealoder
+    //Prealoder
     $(window).on('load', function(event) {
         $('.proloader').delay(500).fadeOut(500);
     });
@@ -18,109 +18,14 @@
             sticky.addClass('sticky');
         }
     });
-
+    //Hamburger menu open
     $('.hamburger-menu').on('click', function() {
         $(this).toggleClass('open');
         $('.main-menu').toggleClass('open');
 
     });
-    /*=========================
-      OwlCarousel START
-    ===========================*/
-
-    // $(".showcase_slider").owlCarousel({
-    //     items: 3,
-    //     nav: true,
-    //     dot: false,
-    //     autoplayTimeout: 2000,
-    //     loop: false,
-    //     navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fas fa-chevron-right"></i>'],
-    //     margin: 4,
-    //     smartSpeed: 450,
-    //     responsiveClass: true,
-    //     responsive: {
-    //         0: {
-    //             items: 1,
-    //             nav: false,
-    //             dot: true
-
-    //         },
-
-    //         768: {
-    //             items: 2,
-
-    //         },
-    //         1000: {
-    //             items: 3,
-
-    //         }
-    //     }
-
-
-    // });
-        $('.owl-carousel').owlCarousel({
-            loop: true,
-            items: 1,
-            slideSpeed: 2000,
-            autoplay: true,
-            thumbs: true,
-            thumbImage: true,
-            thumbContainerClass: 'owl-thumbs',
-            thumbItemClass: 'owl-thumb-item'
-        });
-
-    /* -------------------------------------
-                  Responsive menu
-          -------------------------------------- */
-    var siteMenuClone = function() {
-
-        $('.js-clone-nav').each(function() {
-            var $this = $(this);
-            $this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
-        });
-
-        setTimeout(function() {
-
-            var counter = 0;
-            $('.site-mobile-menu .has-children').each(function() {
-                var $this = $(this);
-
-                $this.prepend('<span class="arrow-collapse collapsed">');
-
-                $this.find('.arrow-collapse').attr({
-                    'data-toggle': 'collapse',
-                    'data-target': '#collapseItem' + counter,
-                });
-
-                $this.find('> ul').attr({
-                    'class': 'collapse',
-                    'id': 'collapseItem' + counter,
-                });
-
-                counter++;
-
-            });
-
-        }, 1000);
-
-        $('body').on('click', '.js-menu-toggle', function(e) {
-            var $this = $(this);
-            e.preventDefault();
-
-            if ($('body').hasClass('offcanvas-menu')) {
-                $('body').removeClass('offcanvas-menu');
-                $this.removeClass('active');
-            } else {
-                $('body').addClass('offcanvas-menu');
-                $this.addClass('active');
-            }
-        })
-
-    };
-    siteMenuClone();
-
-
-    // Show or hide the sticky footer button
+   
+    // Show or hide the scroll-to-top button
     $(window).on('scroll', function(event) {
         if ($(this).scrollTop() > 600) {
             $('.back-to-top').fadeIn(200)
@@ -130,7 +35,7 @@
     });
 
 
-    //Animate the scroll to yop
+    //Animate the scroll to top
     $('.back-to-top').on('click', function(event) {
         event.preventDefault();
 
